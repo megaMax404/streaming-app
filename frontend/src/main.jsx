ec3b3e7883f5b06e4707f571dba6ce3165454ff3
megaMax404 <tanaseklamai@gmail.com>
2026-06-22 15:08:22 +0700

    Recode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-console.log("MAIN JSX LOADED");
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <h1 style={{ color: "white" }}>
-    ROOT WORKS
-  </h1>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
