32211826654ce77a7a7e44852237f217d44bf6eb
megaMax404 <tanaseklamai@gmail.com>
2026-07-22 14:42:57 +0700

    Add GitHub Actions auto deploy v1

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
+set -x
+
+echo "STEP 1"
+
 cd /root/streaming-app || exit
 
+echo "STEP 2"
+
 git fetch origin
+
+echo "STEP 3"
+
 git reset --hard origin/master
 
-##############################
-# FRONTEND
-##############################
+echo "STEP 4"
 
 cd frontend || exit
 
-echo "VITE_API_URL=https://streaming-backend-yzfm.onrender.com" > .env.production
+echo "STEP 5"
 
 npm install
+
+echo "STEP 6"
+
 npm run build
 
+echo "STEP 7"
+
 rm -rf /var/www/doohd/*
+
+echo "STEP 8"
+
 cp -r dist/* /var/www/doohd/
 
-##############################
-# BACKEND
-##############################
-
-cd ../backend || exit
-
-npm install
-
-pm2 restart doohd-api
+echo "STEP 9"
 
 systemctl reload nginx
 
-echo "Deploy Complete"
+echo "STEP 10"
+
+cd ../backend || exit
+
+echo "STEP 11"
+
+pm2 restart doohd-api
+
+echo "DONE"
